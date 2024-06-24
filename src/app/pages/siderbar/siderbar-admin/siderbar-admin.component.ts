4d1a79ad8c9202cebe99c687677cf47971b5aa8d
Kiswendsida <inaodettekab215@gamil.com>
2024-06-24 14:33:17 +0200

    Création de la page login et la mise en forme ( ajout des différents champs)dans le repertoire app/pages/authentification

---
+++ src/app/pages/siderbar/siderbar-admin/siderbar-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-siderbar-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './siderbar-admin.component.html',
+  styleUrl: './siderbar-admin.component.css'
+})
+export class SiderbarAdminComponent {
+
+}
