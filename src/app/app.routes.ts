92047760bce9a94bd5ef64ee979d05ac812fa707
Kiswendsida <inaodettekab215@gamil.com>
2024-06-25 11:22:33 +0200

    création du dasbord admin dans

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/authentification/login/login.component';
+import { SiderbarAdminComponent } from './pages/siderbar/siderbar-admin/siderbar-admin.component';
 
 export const routes: Routes = [
     {
         path:'',
         component:LoginComponent
     }, 
+    {
+        path:'siderbar',
+        component:SiderbarAdminComponent
+    }
 ];
